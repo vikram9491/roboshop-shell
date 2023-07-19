cbfefc2317be225caeed2ef0ed7135e1560b84a1
Raghu K <raghuk.vit@gmail.com>
2023-07-19 06:29:10 +0530

    Frontend Script

--- frontend.sh
+++ frontend.sh
@@ -6,3 +6,5 @@ systemctl start nginx
 rm -rf /usr/share/nginx/html/*
 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
