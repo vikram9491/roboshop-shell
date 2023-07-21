708b9329bd5541a4f8c04f6aa15276f416d50647
Raghu K <raghuk.vit@gmail.com>
2023-07-21 06:18:06 +0530

    RoboShop Scripts Update DNS Records

--- catalogue.sh
+++ catalogue.sh
@@ -6,14 +6,14 @@ yum install nodejs -y
 useradd roboshop
 mkdir /app
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
-cd /app
-unzip /tmp/catalogue.zip
-cd /app
-npm install
-
-yum install mongodb-org-shell -y
-mongo --host mongodb.rdevopsb72.online </app/schema/catalogue.js
-
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl restart catalogue
+#cd /app
+#unzip /tmp/catalogue.zip
+#cd /app
+#npm install
+#
+#yum install mongodb-org-shell -y
+#mongo --host mongodb.rdevopsb72.online </app/schema/catalogue.js
+#
+#systemctl daemon-reload
+#systemctl enable catalogue
+#systemctl restart catalogue
--- mongodb.sh
+++ mongodb.sh
@@ -1,5 +1,5 @@
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org -y
-# Update Listen address from 127.0.0.1 to 0.0.0.0
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 systemctl enable mongod
 systemctl restart mongod
