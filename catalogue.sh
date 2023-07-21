f0acdb93475daa3e51d78b3b6f296969ae03e50b
Raghu K <raghuk.vit@gmail.com>
2023-07-21 06:19:32 +0530

    RoboShop Scripts Update DNS Records

--- catalogue.sh
+++ catalogue.sh
@@ -7,7 +7,7 @@ useradd roboshop
 mkdir /app
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
-#unzip /tmp/catalogue.zip
+unzip /tmp/catalogue.zip
 #cd /app
 #npm install
 #
