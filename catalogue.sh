c19410cbc2d0f94108635d09b4564c2afef9280b
Raghu K <raghuk.vit@gmail.com>
2023-07-21 06:25:48 +0530

    RoboShop Scripts Update DNS Records

--- catalogue.sh
+++ catalogue.sh
@@ -1,19 +1,39 @@
+echo ">>>>>>>>>>>>  Create Catalogue Service  <<<<<<<<<<<<"
 cp catalogue.service /etc/systemd/system/catalogue.service
+
+echo ">>>>>>>>>>>>  Create MongoDB Repo  <<<<<<<<<<<<"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 
+echo ">>>>>>>>>>>>  Install NodeJS Repos  <<<<<<<<<<<<"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+echo ">>>>>>>>>>>>  Install NodeJS  <<<<<<<<<<<<"
 yum install nodejs -y
+
+echo ">>>>>>>>>>>>  Create Application User  <<<<<<<<<<<<"
 useradd roboshop
+
+echo ">>>>>>>>>>>>  Create Application Directory  <<<<<<<<<<<<"
 mkdir /app
+
+echo ">>>>>>>>>>>>  Download Application Content  <<<<<<<<<<<<"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+
+echo ">>>>>>>>>>>>  Extract Application Content  <<<<<<<<<<<<"
 cd /app
 unzip /tmp/catalogue.zip
-#cd /app
-#npm install
-#
-#yum install mongodb-org-shell -y
-#mongo --host mongodb.rdevopsb72.online </app/schema/catalogue.js
-#
-#systemctl daemon-reload
-#systemctl enable catalogue
-#systemctl restart catalogue
+cd /app
+
+echo ">>>>>>>>>>>>  Download NodeJS Dependencies  <<<<<<<<<<<<"
+npm install
+
+echo ">>>>>>>>>>>>  INstall Mongo Client  <<<<<<<<<<<<"
+yum install mongodb-org-shell -y
+
+echo ">>>>>>>>>>>>  Load Catalogue Schema  <<<<<<<<<<<<"
+mongo --host mongodb.rdevopsb72.online </app/schema/catalogue.js
+
+echo ">>>>>>>>>>>>  Start Catalogue Service  <<<<<<<<<<<<"
+systemctl daemon-reload
+systemctl enable catalogue
+systemctl restart catalogue
