457a05d7b8a3515405650c51bd70e4cc511a5381
Raghu K <raghuk.vit@gmail.com>
2023-07-24 07:18:44 +0530

    Password NFR

--- common.sh
+++ common.sh
@@ -119,6 +119,8 @@ func_python() {
 
   func_apppreq
 
+  sed -i "s/rabbitmq_app_password/${rabbitmq_app_password}/" /etc/systemd/system/${component}.service
+
   echo -e "\e[36m>>>>>>>>>>>>  Build ${component} Service   <<<<<<<<<<<<\e[0m"
   pip3.6 install -r requirements.txt &>>${log}
   func_exit_status
