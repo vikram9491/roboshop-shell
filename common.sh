afc18d861c3f7a1012ae2c6459299dd7c81f9cf3
Raghu K <raghuk.vit@gmail.com>
2023-07-24 06:56:56 +0530

    Exit status

--- common.sh
+++ common.sh
@@ -14,7 +14,10 @@ func_apppreq() {
   func_exit_status
 
   echo -e "\e[36m>>>>>>>>>>>>  Create Application User  <<<<<<<<<<<<\e[0m"
-  useradd roboshop &>>${log}
+  id roboshop &>>${log}
+  if [ $? -ne 0 ]; then
+    useradd roboshop &>>${log}
+  fi
   func_exit_status
 
   echo -e "\e[36m>>>>>>>>>>>>  Cleanup Existing Application Content  <<<<<<<<<<<<\e[0m"
